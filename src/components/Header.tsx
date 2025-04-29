beee16e3ce3aa116067cee304162f6ef02d7645a
tarunsrinivas4302 <110880962+tarunsrinivas4302@users.noreply.github.com>
2025-04-29 10:05:46 +0530

    added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
             animate={{ opacity: 1 }}
             className="text-2xl font-bold text-indigo-600"
           >
-            Portofolio
+            <img src='/logo.jpg' alt="TS"/>
           </motion.div>
 
           {/* Desktop Navigation */}
