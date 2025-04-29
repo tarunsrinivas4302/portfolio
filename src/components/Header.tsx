ab42bb6d6c9ccbe76cc954f52ac57400d73cf1bc
tarunsrinivas4302 <110880962+tarunsrinivas4302@users.noreply.github.com>
2025-04-29 10:07:49 +0530

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
             animate={{ opacity: 1 }}
             className="text-2xl font-bold text-indigo-600"
           >
-            <img src='/logo.jpg' alt="TS"/>
+            <img src='/logo.jpg' alt="TS" className="w-14"/>
           </motion.div>
 
           {/* Desktop Navigation */}
